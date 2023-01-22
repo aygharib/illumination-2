e67deab1405d8ec8017107ca5f781a30d8f54276
Ahmad Gharib <12767433+aygharib@users.noreply.github.com>
2023-01-22 17:41:32 -0500

    Replace sdl/2.26.1 with sfml/2.5.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+#include <SFML/Window.hpp>
+
+constexpr int SCREEN_WIDTH = 640;
+constexpr int SCREEN_HEIGHT = 480;
+
 auto main() -> int {
+    sf::Window window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Hello world!");
+
+    sf::sleep(sf::seconds(3));
+
     return 0;
 }
